51d31e3633df768458468eb620c22d37d2623a25
Dota2Beta <shadowdaniil09@gmail.com>
2026-03-03 16:24:41 +0300

    Keep only existing-site links in footer

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
             <div class="col-12 col-lg-3">
                 <div class="footer-brand-card">
                     <h5 class="footer-brand mb-2">HARDZONE</h5>
-                    <p class="footer-brand-text mb-3">Магазин игровых компьютеров, рабочих станций и комплектующих с поддержкой под ваш бюджет.</p>
+                    <p class="footer-brand-text mb-3">Магазин игровых компьютеров и комплектующих с поддержкой под ваш бюджет.</p>
                     <div class="footer-contacts">
                         <div>8 (800) 775-82-35</div>
                         <a href="<?= url('/feedback.php') ?>">Написать в поддержку</a>
@@ -13,50 +13,31 @@
                 </div>
             </div>
 
-            <div class="col-6 col-lg-2">
-                <h6 class="footer-title">Игровые ПК</h6>
+            <div class="col-6 col-lg-3">
+                <h6 class="footer-title">Разделы</h6>
                 <ul class="footer-links">
-                    <li><a href="<?= url('/catalog.php') ?>">Все модели</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">В наличии</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">Конфигуратор</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">Подбор</a></li>
-                </ul>
-            </div>
-
-            <div class="col-6 col-lg-2">
-                <h6 class="footer-title">Серверы</h6>
-                <ul class="footer-links">
-                    <li><a href="<?= url('/catalog.php') ?>">GPU-серверы</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">Для бизнеса</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">ИИ и ML</a></li>
-                </ul>
-
-                <h6 class="footer-title mt-4">Аксессуары</h6>
-                <ul class="footer-links">
-                    <li><a href="<?= url('/catalog.php') ?>">Мониторы</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">Клавиатуры</a></li>
-                    <li><a href="<?= url('/catalog.php') ?>">Мышки</a></li>
-                </ul>
-            </div>
-
-            <div class="col-6 col-lg-2">
-                <h6 class="footer-title">Покупателям</h6>
-                <ul class="footer-links">
-                    <li><a href="<?= url('/cart.php') ?>">Этапы заказа</a></li>
-                    <li><a href="<?= url('/cart.php') ?>">Оплата</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Доставка</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Гарантия</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Подарочные сертификаты</a></li>
+                    <li><a href="<?= url('/index.php') ?>">Главная</a></li>
+                    <li><a href="<?= url('/catalog.php') ?>">Каталог</a></li>
+                    <li><a href="<?= url('/cart.php') ?>">Корзина</a></li>
+                    <li><a href="<?= url('/about.php') ?>">О нас</a></li>
                 </ul>
             </div>
 
             <div class="col-6 col-lg-3">
-                <h6 class="footer-title">Компания</h6>
+                <h6 class="footer-title">Пользователь</h6>
                 <ul class="footer-links">
-                    <li><a href="<?= url('/about.php') ?>">О нас</a></li>
+                    <li><a href="<?= url('/profile.php') ?>">Профиль</a></li>
+                    <li><a href="<?= url('/login.php') ?>">Вход</a></li>
+                    <li><a href="<?= url('/register.php') ?>">Регистрация</a></li>
+                    <li><a href="<?= url('/logout.php') ?>">Выход</a></li>
+                </ul>
+            </div>
+
+            <div class="col-12 col-lg-3">
+                <h6 class="footer-title">Коммуникация</h6>
+                <ul class="footer-links">
                     <li><a href="<?= url('/reviews.php') ?>">Отзывы</a></li>
-                    <li><a href="<?= url('/feedback.php') ?>">Контакты</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Вакансии</a></li>
+                    <li><a href="<?= url('/feedback.php') ?>">Обратная связь</a></li>
                 </ul>
             </div>
         </div>
@@ -67,7 +48,7 @@
 
         <div class="footer-bottom mt-4 pt-3">
             <span>Copyright © 2010-<?= date('Y') ?> HARDZONE.</span>
-            <span><a href="#">Правовая информация</a> | <a href="#">Карта сайта</a></span>
+            <span><a href="<?= url('/about.php') ?>">О компании</a> | <a href="<?= url('/catalog.php') ?>">Каталог товаров</a></span>
         </div>
     </div>
 </footer>
