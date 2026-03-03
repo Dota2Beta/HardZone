deb43879c0c405c2ac5bec5ccc686efbea372311
Dota2Beta <shadowdaniil09@gmail.com>
2026-03-03 16:16:55 +0300

    Refine footer visuals to match HardZone theme

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,18 @@
 </main>
 <footer class="hz-footer mt-5">
     <div class="container py-5">
-        <div class="row g-4">
+        <div class="row g-4 align-items-start">
+            <div class="col-12 col-lg-3">
+                <div class="footer-brand-card">
+                    <h5 class="footer-brand mb-2">HARDZONE</h5>
+                    <p class="footer-brand-text mb-3">Магазин игровых компьютеров, рабочих станций и комплектующих с поддержкой под ваш бюджет.</p>
+                    <div class="footer-contacts">
+                        <div>8 (800) 775-82-35</div>
+                        <a href="<?= url('/feedback.php') ?>">Написать в поддержку</a>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-6 col-lg-2">
                 <h6 class="footer-title">Игровые ПК</h6>
                 <ul class="footer-links">
@@ -11,6 +22,7 @@
                     <li><a href="<?= url('/catalog.php') ?>">Подбор</a></li>
                 </ul>
             </div>
+
             <div class="col-6 col-lg-2">
                 <h6 class="footer-title">Серверы</h6>
                 <ul class="footer-links">
@@ -18,6 +30,7 @@
                     <li><a href="<?= url('/catalog.php') ?>">Для бизнеса</a></li>
                     <li><a href="<?= url('/catalog.php') ?>">ИИ и ML</a></li>
                 </ul>
+
                 <h6 class="footer-title mt-4">Аксессуары</h6>
                 <ul class="footer-links">
                     <li><a href="<?= url('/catalog.php') ?>">Мониторы</a></li>
@@ -25,16 +38,8 @@
                     <li><a href="<?= url('/catalog.php') ?>">Мышки</a></li>
                 </ul>
             </div>
+
             <div class="col-6 col-lg-2">
-                <h6 class="footer-title">Услуги</h6>
-                <ul class="footer-links">
-                    <li><a href="<?= url('/about.php') ?>">Модернизация</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Трейд-ин</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Профилактика</a></li>
-                    <li><a href="<?= url('/about.php') ?>">Аренда ПК</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-lg-3">
                 <h6 class="footer-title">Покупателям</h6>
                 <ul class="footer-links">
                     <li><a href="<?= url('/cart.php') ?>">Этапы заказа</a></li>
@@ -44,6 +49,7 @@
                     <li><a href="<?= url('/about.php') ?>">Подарочные сертификаты</a></li>
                 </ul>
             </div>
+
             <div class="col-6 col-lg-3">
                 <h6 class="footer-title">Компания</h6>
                 <ul class="footer-links">
@@ -56,7 +62,7 @@
         </div>
 
         <div class="footer-note mt-4">
-            <span>Приглашаем вас в наш <a href="<?= url('/about.php') ?>">шоу-рум</a>. Позвонить 8 (800) 775-82-35 или <a href="<?= url('/feedback.php') ?>">написать нам</a>.</span>
+            Приглашаем вас в наш <a href="<?= url('/about.php') ?>">шоу-рум</a>. Поможем собрать систему под игры, стриминг, дизайн или работу с AI.
         </div>
 
         <div class="footer-bottom mt-4 pt-3">
